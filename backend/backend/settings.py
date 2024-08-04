474b19c68423df403855c87092c55243c66b493c
Роман Турсков <roman.turskov@yandex.ru>
2024-08-04 18:21:33 +0300

    Add domen

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
+import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
-DEBUG = True
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 
 # Application definition
